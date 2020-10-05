e22c5c4646c57d6ae6c360356379ea13562bd013
xu3s <okniesutrisno@gmail.com>
2020-10-05 08:38:09 +0700

    1'

--- dbconnect.py
+++ dbconnect.py
@@ -1,6 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 Test
 =======
+=======
+>>>>>>> 3919844... init
 import MySQLdb
 import config
 
@@ -22,4 +25,7 @@ def connection():
     c = conn.cursor()
 
     return c, conn
+<<<<<<< HEAD
 >>>>>>> aaffeff... shhh
+=======
+>>>>>>> 3919844... init
